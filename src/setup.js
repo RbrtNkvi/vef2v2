5e61b8df66c6db3f0d8f3fb43eb371b7a63f5e49
unknown <robert_nokkvi@hotmail.com>
2022-02-18 22:55:00 +0000

    setup fix

--- src/setup.js
+++ src/setup.js
@@ -1,14 +1,12 @@
-import { createSchema, dropSchema, insertSchema } from './lib/db.js';
+import { createSchema, dropSchema} from './lib/db.js';
 import { createUser } from './lib/users.js';
 
 const SCHEMA_FILE = './sql/schema.sql';
 const DROP_SCHEMA_FILE = './sql/drop.sql';
-const INSERT_SCHEMA_FILE = './sql/insert.sql';
 
 async function create() {
   dropSchema(DROP_SCHEMA_FILE);
   createSchema(SCHEMA_FILE);
-  insertSchema(INSERT_SCHEMA_FILE);
   createUser('admin', '123');
 }
 
